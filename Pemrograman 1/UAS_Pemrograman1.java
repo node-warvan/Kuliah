3c37347357454097f2c30591560a546eb4434283
WARVAN <122137078+node-warvan@users.noreply.github.com>
2023-12-19 20:09:04 +0700

    Create UAS_Pemrograman1.java

---
+++ Pemrograman 1/UAS_Pemrograman1.java
@@ -0,0 +1,22 @@
+public class Biodata {
+    public static void main(String[] args) {
+        // Data biodata
+        String namaLengkap = "Irvan Fauzi";
+        String nim = "211011450005";
+        String programStudi = "Teknik Informatika";
+        String ruangKelas = "617";
+        String semester = "4";
+        String noHp = "085714531812";
+        String fotoAsli = "Nama file foto Anda";
+
+        // Menampilkan biodata
+        System.out.println("UAS PEMROGRAMAN 1 JAVA PROGRAMMING");
+        System.out.println("Nama Lengkap\t= " + namaLengkap);
+        System.out.println("NIM\t\t= " + nim);
+        System.out.println("Program Studi\t= " + programStudi);
+        System.out.println("Ruang Kelas\t= " + ruangKelas);
+        System.out.println("Semester\t= " + semester);
+        System.out.println("No Hp\t\t= " + noHp);
+        System.out.println("Foto Asli\t= " + fotoAsli);
+    }
+}
